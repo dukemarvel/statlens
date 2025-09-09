d8f5c701d0a08f9d7929c86898d007b4f95d798c
Joseph BlackDuke <josephblackduke@icloud.com>
2025-09-09 17:55:47 +0100

    add viewsets for competitions app

--- engine/competitions/views.py
+++ engine/competitions/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .models import Competition, Season
+from .serializers import CompetitionSerializer, SeasonSerializer
 
-# Create your views here.
+class CompetitionViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Competition.objects.all().order_by("country", "name")
+    serializer_class = CompetitionSerializer
+
+class SeasonViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = SeasonSerializer
+
+    def get_queryset(self):
+        qs = Season.objects.select_related("competition").order_by("-year_start")
+        comp = self.request.query_params.get("competition")
+        if comp:
+            qs = qs.filter(competition_id=comp)
+        return qs
