f1ce8b18fe62539d8dd87d89b6386847dcee7429
Joseph BlackDuke <josephblackduke@icloud.com>
2025-09-09 20:17:28 +0100

    add viewsets for users app

--- engine/users/views.py
+++ engine/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions
+from .models import UserView
+from .serializers import UserViewSerializer
 
-# Create your views here.
+class UserViewViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    serializer_class = UserViewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return UserView.objects.filter(user=self.request.user).order_by("-created_at")
