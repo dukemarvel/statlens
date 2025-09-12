0f30b6ce72a7842a87ff7803655b8e38fca1915c
Joseph BlackDuke <josephblackduke@icloud.com>
2025-09-12 23:29:52 +0100

    fix(base.py): code clean up

--- engine/engine/settings/base.py
+++ engine/engine/settings/base.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # local apps
     "competitions",
     "core",
     "h2h",
@@ -54,7 +53,7 @@ ROOT_URLCONF = "engine.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],  # optional but handy
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -91,7 +90,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"  # good default
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
